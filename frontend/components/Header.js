bfef08d666923d42bcf348123e1f9877ef12fb5d
stephmen <stephmen@gmail.com>
2019-03-23 09:53:43 -0400

    firstCommit

---
+++ frontend/components/Header.js
@@ -0,0 +1,14 @@
+import Nav from './Nav';
+const Header = () => (
+  <div>
+    <div className="bar">
+    <a href="">Sick Fits</a>
+    <Nav />
+    </div>
+    <div className="sub-bar"></div>
+    <p>Search</p>
+    <div>Cart</div>
+  </div>
+)
+
+export default Header;
---
+++ frontend/components/Nav.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+const Nav = () => (
+  <div>
+  <Link href="/sell">
+  <a>Sell!TEST</a>
+   </Link>
+   <br></br>
+   <Link href="/">
+     <a >Home!TEST</a>
+  </Link>
+  </div>
+)
+
+export default Nav;
